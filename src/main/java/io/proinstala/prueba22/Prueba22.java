e0091fde695e9156233edb500811210628811da2
proinstala <davidesfalko@gmail.com>
2024-07-04 20:13:24 +0200

    pruebas desde netbeans

---
+++ src/main/java/io/proinstala/prueba22/Prueba22.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.proinstala.prueba22;
+
+/**
+ *
+ * @author David
+ */
+public class Prueba22 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
